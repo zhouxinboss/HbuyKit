46c1552f95e089410497bc94afda7180e48e2969
zhouxingbo <18142825602@163.com>
2021-08-20 13:57:57 +0800

    add file
    Signed-off-by: zhouxingbo <18142825602@163.com>

--- Example/Pods/Target Support Files/HbuyKit/HbuyKit-umbrella.h
+++ Example/Pods/Target Support Files/HbuyKit/HbuyKit-umbrella.h
@@ -11,6 +11,8 @@
 #endif
 
 #import "MyAttributedStringBuilder.h"
+#import "UIButton+LXMImagePosition.h"
+#import "UIView+ExtendTouchRect.h"
 
 FOUNDATION_EXPORT double HbuyKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char HbuyKitVersionString[];
---
+++ HbuyKit/Classes/UIView+ExtendTouchRect.h
@@ -0,0 +1,16 @@
+//
+//  UIView+ExtendTouchRect.h
+//  DragonpassCn
+//
+//  Created by 刘思源 on 16/10/17.
+//  Copyright © 2016年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ExtendTouchRect)
+
+/** uiview的touch响应面积 */
+@property (nonatomic, assign) UIEdgeInsets touchExtendInset;
+
+@end
